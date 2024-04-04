3f953f2a25750a1661a0db160a3ba19ab93662b7
Figby222 <figby222@outlook.com>
2024-04-03 23:49:51 -0700

    RM Gameboard.test.js

--- src/modules/Gameboard.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-/* eslint-disable no-undef */
-import Gameboard from "./Gameboard.js";
-
-describe("Gameboard constructor", () => {
-    test ("It has board property", () => {
-        expect(new Gameboard()).toHaveProperty("board")
-    })
-})
