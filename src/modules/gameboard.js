317f062ca896e25fd6f551413fdf3fa9a22ca98e
Figby222 <figby222@outlook.com>
2024-04-03 23:44:51 -0700

    Add Gameboard.setBoard()

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,6 +2,19 @@ class Gameboard {
     constructor() {
         this.board = [];
     }
+
+    static setBoard() {
+        const board = [];
+        for (let i = 0; i < 10; i+=1) {
+            board.push([]);
+        }
+
+        return board;
+    }
+
+    placeBoat(boat, coordinates) {
+        
+    }
 }
 
 export default Gameboard;
--- src/modules/gameboard.test.js
+++ src/modules/gameboard.test.js
@@ -7,3 +7,24 @@ describe("Gameboard constructor", () => {
         expect(new Gameboard()).toHaveProperty("board")
     })
 })
+
+describe("#set-board", () => {
+    test("setBoard returns a 3d array", () => {
+        const board = Gameboard.setBoard();
+        expect(board[0][0]).not.toBeNull();
+    })
+})
+
+describe.skip("#place-boat", () => {
+    test("fake boat is placed in board", () => {
+        const myBoard = new Gameboard();
+
+        jest.mock("./boat.js", () => {
+            myBoat: true
+        })
+
+        const boat = new Boat(4);
+        myBoard.placeBoat(boat, [0, 1]);
+        expect(myBoard.board[0][1]).toMatch(boat);
+    })
+})
