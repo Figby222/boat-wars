fb8cd36ba5ccace487ca14f71f37ca376b2fdb13
Figby222 <figby222@outlook.com>
2024-04-05 04:21:22 -0700

    Add and test Player.shoot()

--- src/modules/player.js
+++ src/modules/player.js
@@ -2,6 +2,15 @@ class Player {
     constructor() {
         this.shotsArr = [];
     }
+
+    shoot(coordinates) {
+        if (this.shotsArr.includes(JSON.stringify(coordinates))) {
+            throw new Error("Cell has already been shot");
+        }
+
+        this.shotsArr.push(JSON.stringify(coordinates))
+        return coordinates;
+    }
 }
 
 export default Player;
--- src/modules/player.test.js
+++ src/modules/player.test.js
@@ -2,8 +2,32 @@
 /* eslint-disable import/extensions */
 import Player from "./player.js";
 
+let player = new Player();
+
+beforeEach(() => {
+    player = new Player();
+})
 describe("#PlayerConstructor", () => {
     test("It structs obj", () => {
         expect(typeof(new Player())).toBe("object");
+    })
+})
+
+describe("#shoot", () => {
+    test("It returns coordinates", () => {
+        const coordinates = [2, 2];
+        expect(player.shoot(coordinates)).toEqual(coordinates)
+    })
+
+    test("It pushes coordinates to shotsArr", () => { // delete later
+        player.shoot([2, 2]);
+        expect(player.shotsArr).toContainEqual("[2,2]")
+    })
+
+    test("It throws error if coordinates have already been shot", () => {
+        const coordinates = [2, 2]
+        player.shoot(coordinates);
+        expect(() => player.shoot(coordinates))
+            .toThrow(new Error("Cell has already been shot"));
     })
 })
