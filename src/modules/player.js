a81c0e0bd1cae3ba88edc228f81e5596ee794a37
Figby222 <figby222@outlook.com>
2024-04-05 05:08:05 -0700

    Add enemyBoard param to Player.shoot()
    Call enemyBoard.receiveAttack(coordinates) instead
    of returning coordinates

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,13 +3,13 @@ class Player {
         this.shotsArr = [];
     }
 
-    shoot(coordinates) {
+    shoot(enemyBoard, coordinates) {
         if (this.shotsArr.includes(JSON.stringify(coordinates))) {
             throw new Error("Cell has already been shot");
         }
 
         this.shotsArr.push(JSON.stringify(coordinates))
-        return coordinates;
+        enemyBoard.receiveAttack(coordinates);
     }
 }
 
--- src/modules/player.test.js
+++ src/modules/player.test.js
@@ -3,6 +3,7 @@
 import Player from "./player.js";
 
 let player = new Player();
+const MockGameboard = { receiveAttack: () => {}};
 
 beforeEach(() => {
     player = new Player();
@@ -14,20 +15,15 @@ describe("#PlayerConstructor", () => {
 })
 
 describe("#shoot", () => {
-    test("It returns coordinates", () => {
-        const coordinates = [2, 2];
-        expect(player.shoot(coordinates)).toEqual(coordinates)
-    })
-
-    test("It pushes coordinates to shotsArr", () => { // delete later
-        player.shoot([2, 2]);
-        expect(player.shotsArr).toContainEqual("[2,2]")
-    })
+    // test("It pushes coordinates to shotsArr", () => { // delete later
+    //     player.shoot([2, 2]);
+    //     expect(player.shotsArr).toContainEqual("[2,2]")
+    // })
 
     test("It throws error if coordinates have already been shot", () => {
         const coordinates = [2, 2]
-        player.shoot(coordinates);
-        expect(() => player.shoot(coordinates))
+        player.shoot(MockGameboard, coordinates);
+        expect(() => player.shoot(MockGameboard, coordinates))
             .toThrow(new Error("Cell has already been shot"));
     })
 })
