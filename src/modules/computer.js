a5bb6dad9221f021ec83372859b9efe971f802ea
Figby222 <figby222@outlook.com>
2024-04-06 05:36:52 -0700

    Add and test Computer.shoot()
    Use two different methods for Computer.shoot() based in whether
    advancedMode is true or false

--- src/modules/computer.js
+++ src/modules/computer.js
@@ -1,6 +1,27 @@
+/* eslint-disable func-names */
 class Computer {
     constructor(advancedMode = false) {
         this.advancedMode = advancedMode;
+        this.shotsArr = [];
+
+        if (this.advancedMode) {
+            this.shoot = function() {
+                
+            };
+        } else {
+            this.shoot = function(enemyBoard) {
+                const x = Math.floor(Math.random() * 10);
+                const y = Math.floor(Math.random() * 10);
+                const coordinates = [x, y];
+
+                if (this.shotsArr.includes(JSON.stringify(coordinates))) {
+                    return this.shoot(enemyBoard);
+                }
+
+                this.shotsArr.push(JSON.stringify(coordinates));
+                enemyBoard.receiveAttack(coordinates);
+            };
+        }
     }
 }
 
--- src/modules/computer.test.js
+++ src/modules/computer.test.js
@@ -2,6 +2,7 @@
 /* eslint-disable import/extensions */
 import Computer from "./computer.js";
 
+const MockGameboard = {receiveAttack: jest.fn(() => true)};
 describe("Constructor", () => {
     test("It creates object", () => {
         expect(typeof new Computer()).toBe("object");
@@ -9,5 +10,13 @@ describe("Constructor", () => {
 
     test("It creates object with advanced mode", () => {
         expect(new Computer(true).advancedMode).toBe(true);
+    })
+})
+
+describe("#shoot", () => {
+    test("It calls receiveAttack once", () => {
+        const computer = new Computer(false);
+        computer.shoot(MockGameboard);
+        expect (MockGameboard.receiveAttack.mock.calls).toHaveLength(1);
     })
 })
