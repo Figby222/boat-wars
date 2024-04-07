ca148e786f48a009ac3db0b3f4fb80ad8a0aef24
Figby222 <figby222@outlook.com>
2024-04-07 09:17:00 -0700

    Mock DOM

--- src/modules/dom-control/dom-control.test.js
+++ src/modules/dom-control/dom-control.test.js
@@ -2,6 +2,17 @@
 // eslint-disable-next-line import/extensions
 import DOMControl from "./dom-control.js";
 
+const MockDOM = () => ({
+    append: jest.fn(() => {}),
+    addEventListener: () => {},
+    bgColor: "white",
+});
+
+let myDOM = new MockDOM();
+beforeEach(() => {
+    myDOM = new MockDOM();
+})
+
 describe("Constructor", () => {
     test("It exists", () => {
         expect(new DOMControl()).toBeDefined();
