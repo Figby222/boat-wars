9a47bc09dc70a559caedfe25a318529ad446a6e2
Figby222 <figby222@outlook.com>
2024-04-07 06:09:59 -0700

    Import Boat, Computer, Gameboard, and Player

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import Boat from "./modules/boat/boat.js";
+import Computer from "./modules/computer/computer.js";
+import Gameboard from "./modules/gameboard/gameboard.js";
+import Player from "./modules/player/player.js";
