81e3b4284d2ed4194aa9821af2165af5bf46ee7e
Figby222 <figby222@outlook.com>
2024-04-07 06:51:48 -0700

    Set up player1, computer, and their Gameboards

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import Boat from "./modules/boat/boat.js";
 import Computer from "./modules/computer/computer.js";
 import Gameboard from "./modules/gameboard/gameboard.js";
 import Player from "./modules/player/player.js";
+
+const player1 = new Player();
+const computer = new Computer(false);
+const player1Board = new Gameboard();
+const computerBoard = new Gameboard();
+
+player1Board.placeBoat(new Boat(4), [6, 4], "y");
+player1Board.placeBoat(new Boat(1), [3, 3]);
+player1Board.placeBoat(new Boat(2), [1, 1]);
+player1Board.placeBoat(new Boat(3), [7, 6], "y");
+
+computerBoard.placeBoat(new Boat(4), [6, 4], "y");
+computerBoard.placeBoat(new Boat(1), [3, 3]);
+computerBoard.placeBoat(new Boat(2), [1, 1]);
+computerBoard.placeBoat(new Boat(3), [7, 6], "y")
