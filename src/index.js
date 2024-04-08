349fe38df27304107b2e46f94b2468371e667625
Figby222 <figby222@outlook.com>
2024-04-08 08:38:00 -0700

    Rename computer -> player2
    Rename computerBoard -> player2Board

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import Player from "./modules/player/player.js";
 import DOMControl from "./modules/dom-control/dom-control.js";
 
 const player1 = new Player();
-const computer = new Computer(false);
+const player2 = new Computer(false);
 const player1Board = new Gameboard();
-const computerBoard = new Gameboard();
+const player2Board = new Gameboard();
 const DOMCtrl = new DOMControl();
 
 let currentTurn = player1;
@@ -19,13 +19,13 @@ player1Board.placeBoat(new Boat(1), [3, 3]);
 player1Board.placeBoat(new Boat(2), [1, 1]);
 player1Board.placeBoat(new Boat(3), [7, 6], "y");
 
-computerBoard.placeBoat(new Boat(4), [6, 4], "y");
-computerBoard.placeBoat(new Boat(1), [3, 3]);
-computerBoard.placeBoat(new Boat(2), [1, 1]);
-computerBoard.placeBoat(new Boat(3), [7, 6], "y")
+player2Board.placeBoat(new Boat(4), [6, 4], "y");
+player2Board.placeBoat(new Boat(1), [3, 3]);
+player2Board.placeBoat(new Boat(2), [1, 1]);
+player2Board.placeBoat(new Boat(3), [7, 6], "y")
 
 DOMCtrl.renderGameboard(player1Board, "player1", true);
-DOMCtrl.renderGameboard(computerBoard, "player2", false);
+DOMCtrl.renderGameboard(player2Board, "player2", false);
 
 player1Board.receiveAttack([0, 1]);
 DOMCtrl.renderGameboard(player1Board, "player1", true);
