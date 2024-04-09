6618a51e8cf6be6560e9cbc2df7071e26eab4b85
Figby222 <figby222@outlook.com>
2024-04-09 16:31:45 -0700

    Init new Gameboard for each player

--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@ import DOMControl from "./modules/dom-control/dom-control.js";
 
 const player1 = new Player();
 const player2 = new Computer(false);
-const player1Board = new Gameboard();
-const player2Board = new Gameboard();
+player1.gameboard = new Gameboard();
+player2.gameboard = new Gameboard();
 const DOMCtrl = new DOMControl();
 
 let currentTurn = player1;
 let continueGame = true;
 
-player1Board.placeBoat(new Boat(4), [6, 4], "y");
-player1Board.placeBoat(new Boat(1), [3, 3]);
-player1Board.placeBoat(new Boat(2), [1, 1]);
-player1Board.placeBoat(new Boat(3), [7, 6], "y");
+player1.gameboard.board.placeBoat(new Boat(4), [6, 4], "y");
+player1.gameboard.board.placeBoat(new Boat(1), [3, 3]);
+player1.gameboard.board.placeBoat(new Boat(2), [1, 1]);
+player1.gameboard.board.placeBoat(new Boat(3), [7, 6], "y");
 
-player2Board.placeBoat(new Boat(4), [6, 4], "y");
-player2Board.placeBoat(new Boat(1), [3, 3]);
-player2Board.placeBoat(new Boat(2), [1, 1]);
-player2Board.placeBoat(new Boat(3), [7, 6], "y")
+player2.gameboard.board.placeBoat(new Boat(4), [6, 4], "y");
+player2.gameboard.board.placeBoat(new Boat(1), [3, 3]);
+player2.gameboard.board.placeBoat(new Boat(2), [1, 1]);
+player2.gameboard.board.placeBoat(new Boat(3), [7, 6], "y")
 
 if (currentTurn === player1) {
-    DOMCtrl.renderGameboard(player1Board, "player1", true);
-    DOMCtrl.renderGameboard(player2Board, "player2", false);
+    DOMCtrl.renderGameboard(player1.gameboard, "player1", true);
+    DOMCtrl.renderGameboard(player2.gameboard, "player2", false);
 
 } else {
-    DOMCtrl.renderGameboard(player1Board, "player1", false);
-    DOMCtrl.renderGameboard(player2Board, "player2", true);
+    DOMCtrl.renderGameboard(player1.gameboard, "player1", false);
+    DOMCtrl.renderGameboard(player2.gameboard, "player2", true);
 }
