a1c79492fd1338de1c131750b3261211311de960
Figby222 <figby222@outlook.com>
2024-04-10 00:12:39 -0700

    RM renderGameboardVisible() & getInput()

--- src/modules/dom-control/dom-control.js
+++ src/modules/dom-control/dom-control.js
@@ -60,49 +60,49 @@ DOMControl.prototype.renderGameboard = function(player, id, setVisible = false) 
     }
 }
 
-DOMControl.prototype.renderGameboardVisible = function(gameboard, id) {
-    const boardContainer = document.querySelector(`#${id}`);
-    boardContainer.textContent = "";
+// DOMControl.prototype.renderGameboardVisible = function(gameboard, id) {
+//     const boardContainer = document.querySelector(`#${id}`);
+//     boardContainer.textContent = "";
 
-    for (let x = 0; x < gameboard.board.length; x+=1) {
-        for (let y = 0; y < gameboard.board.length; y+=1) {
-            const cellContainer = document.createElement("div");
-            cellContainer.classList.add("cell");
-
-            
-            if (setVisible) {
-                if (gameboard.board[x][y] === false) {
-                    cellContainer.classList.add("miss");
-                } else if (gameboard.board[x][y] != null) {
-                    cellContainer.classList.add("boat");
-                    cellContainer.classList.add("visible");
-                    if (gameboard.board[x][y] === "destroyed") {
-                        cellContainer.classList.add("hit");
-                    }
-
-                }
-            }
+//     for (let x = 0; x < gameboard.board.length; x+=1) {
+//         for (let y = 0; y < gameboard.board.length; y+=1) {
+//             const cellContainer = document.createElement("div");
+//             cellContainer.classList.add("cell");
 
             
-            if (cellContainer.classList.contains("notShot")) {
-                cellContainer.addEventListener("click", () => {
-                    gameboard.receiveAttack([x, y]);
-                    this.renderGameboard(gameboard, id, setVisible);
-                })
-            }
-            boardContainer.appendChild(cellContainer);
-        }
-    }
-}
+//             if (setVisible) {
+//                 if (gameboard.board[x][y] === false) {
+//                     cellContainer.classList.add("miss");
+//                 } else if (gameboard.board[x][y] != null) {
+//                     cellContainer.classList.add("boat");
+//                     cellContainer.classList.add("visible");
+//                     if (gameboard.board[x][y] === "destroyed") {
+//                         cellContainer.classList.add("hit");
+//                     }
 
-DOMControl.prototype.getInput = function(playerid) {
-    const cellList = document.querySelector(`#${playerid}`);
-    Array.from(cellList);
-    cellList.forEach((cell) => {
-        cell.addEventListener("click", () => {
-            return cell.coordinates;
-        })
-    })
-}
+//                 }
+//             }
+
+            
+//             if (cellContainer.classList.contains("notShot")) {
+//                 cellContainer.addEventListener("click", () => {
+//                     gameboard.receiveAttack([x, y]);
+//                     this.renderGameboard(gameboard, id, setVisible);
+//                 })
+//             }
+//             boardContainer.appendChild(cellContainer);
+//         }
+//     }
+// }
+
+// DOMControl.prototype.getInput = function(playerid) {
+//     const cellList = document.querySelector(`#${playerid}`);
+//     Array.from(cellList);
+//     cellList.forEach((cell) => {
+//         cell.addEventListener("click", () => {
+//             return cell.coordinates;
+//         })
+//     })
+// }
 
 export default DOMControl;
