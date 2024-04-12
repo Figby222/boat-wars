9491649e71512bf7bed74924dcf6772ff4b66070
Figby222 <figby222@outlook.com>
2024-04-12 05:01:16 -0700

    Add Player.name

--- src/modules/player/player.js
+++ src/modules/player/player.js
@@ -1,6 +1,7 @@
-const Player = function(gameboard) {
+const Player = function(gameboard, name) {
     this.gameboard = gameboard;
     this.isTurn = false;
+    this.name = name;
 }
 
 Player.prototype.shoot = function(enemyBoard, coordinates) {
