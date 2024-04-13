a37a39512e9213c44c2b2e615d4fb37ed9f42972
Figby222 <figby222@outlook.com>
2024-04-13 00:35:00 -0700

    Line 7-8: Change const init to this

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -4,8 +4,8 @@ import Computer from "../computer/computer.js";
 
 class Game {
     constructor(player1Name, player2Name = "computer") {
-        const player1 = new Player(new Gameboard(), player1Name);
-        const player2 = new Player(new Gameboard(), player2Name);
+        this.player1 = new Player(new Gameboard(), player1Name);
+        this.player2 = new Player(new Gameboard(), player2Name);
     }
 
     checkGameOver() {
