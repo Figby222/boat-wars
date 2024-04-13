4fa4ba2c49351fdbe7b2644712d7ec80ca5ae6aa
Figby222 <figby222@outlook.com>
2024-04-12 23:51:21 -0700

    Create new Game before each test

--- src/modules/game/game.test.js
+++ src/modules/game/game.test.js
@@ -1,2 +1,9 @@
 // eslint-disable-next-line import/extensions
 import Game from "./game.js";
+
+beforeEach(() => {
+    const game = new Game(false);
+})
+test("playRound shoots gameboard", () => {
+    
+})
