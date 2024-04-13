a0068fff870ab2048b1c931f722e107fce857a60
Figby222 <figby222@outlook.com>
2024-04-13 01:47:51 -0700

    Add game and display

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import DOMControl from "./modules/dom-control/dom-control.js";
 import Game from "./modules/game/game.js";
 
 
+const game = new Game("player1", "player2");
+
+const display = new DOMControl(game);
