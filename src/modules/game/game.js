7380d5cd906041712919637ea386a131792d446f
Figby222 <figby222@outlook.com>
2024-04-13 23:32:34 -0700

    Add display param to Game constructor

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -3,7 +3,8 @@ import Gameboard from "../gameboard/gameboard.js";
 import Computer from "../computer/computer.js";
 
 class Game {
-    constructor(player1Name, player2Name = "computer") {
+    constructor(display, player1Name, player2Name = "computer") {
+        this.display = display;
         this.player1 = new Player(new Gameboard(), player1Name);
         this.player2 = new Player(new Gameboard(), player2Name);
 
