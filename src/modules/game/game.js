27a982de8146506f05566ff5e63f56b4fb1403fb
Figby222 <figby222@outlook.com>
2024-04-13 23:53:43 -0700

    Render each gameboard in each round

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -19,9 +19,13 @@ class Game {
         if (this.currentTurn === this.player1) {
             const enemyBoard = this.player2.gameboard;
             this.player1.shoot(enemyBoard, coordinates);
+            this.display.renderPlayer1Gameboard(this, this.player1.gameboard, false);
+            this.display.renderPlayer2Gameboard(this, this.player2.gameboard, true);
         } else {
             const enemyBoard = this.player1.gameboard;
             this.player2.shoot(enemyBoard, coordinates);
+            this.display.renderPlayer1Gameboard(this, this.player1.gameboard, true);
+            this.display.renderPlayer2Gameboard(this, this.player2.gameboard, false);
         }
         
         this.switchTurn();
