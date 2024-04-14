1c64faf50650cf7a512707e6f27397323c5e9d26
Figby222 <figby222@outlook.com>
2024-04-13 23:58:19 -0700

    Index.js: render gameboards with updated args

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const display = new DOMControl();
 const game = new Game(display, "player1", "player2");
 
 display.placeBoats(game.player1.gameboard, game.player2.gameboard);
-display.renderPlayer1Gameboard(true);
-display.renderPlayer2Gameboard(false);
+display.renderPlayer1Gameboard(game, game.player1.gameboard, true);
+display.renderPlayer2Gameboard(game, game.player2.gameboard, false);
