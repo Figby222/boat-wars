44371a6b9091ad1dae772a87de5e2ce963936a62
Figby222 <figby222@outlook.com>
2024-04-15 08:34:35 -0700

    Fix getAdjacentCellCoordinates() return value

--- src/modules/gameboard/gameboard.js
+++ src/modules/gameboard/gameboard.js
@@ -85,14 +85,14 @@ class Gameboard {
     getAdjacentCellCoordinates(coordinates) {
         const [x, y] = coordinates;
         return [
-            [x - 1][y],
-            [x + 1], [y],
-            [x][y - 1],
-            [x][y + 1],
-            [x - 1][y - 1],
-            [x + 1][y- 1],
-            [x - 1][y + 1],
-            [x + 1][y + 1],
+            [x - 1, y],
+            [x + 1, y],
+            [x, y - 1],
+            [x, y + 1],
+            [x - 1, y - 1],
+            [x + 1, y- 1],
+            [x - 1, y + 1],
+            [x + 1, y + 1],
         ]
     }
 
