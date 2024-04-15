44fe0657cc6f0f243ef6f5244f2d515551d475d3
Figby222 <figby222@outlook.com>
2024-04-15 09:17:02 -0700

    Fix typo at line 84

--- src/modules/gameboard/gameboard.js
+++ src/modules/gameboard/gameboard.js
@@ -81,7 +81,7 @@ class Gameboard {
             adjacentCells.forEach((coordinates) => {
                 console.log(coordinates);
                 if (!this.checkIfOutOfBounds(coordinates) && !(this.getCell(coordinates).value instanceof Boat)) {
-                    this.getCell(coordinates).value = "unavaileble";
+                    this.getCell(coordinates).value = "unavailable";
                 }
             })
 
